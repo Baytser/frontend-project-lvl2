df0b03dbd5d4b9c978eaa327c66a3f255089c746
Baitser Serhii <Baytsers@gmail.com>
2019-12-24 14:10:17 +0200

    Step 2

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
-import program from 'commander'
+import program from 'commander';
+
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig> <secondConfig>')
+  .option('-f, --format [type] ', 'Output format')
+  .action((firstPath, secondPath) => {
+    console.log(`Test gendiff ${firstPath} ${secondPath}`);
+  })
   .parse(process.argv);
